12d38971314dc52bbf9361e6a9e92c41c319f696
James Lee-Jones <james@lee-jones.co.uk>
2026-02-07 21:59:33 +0000

    fix(lambda): fix printing of lambda abstractions
    Print lambda abstractions as (\x. ...) to ensure round trip proprty
    holds.

--- lib/lambda/print.ml
+++ lib/lambda/print.ml
@@ -5,7 +5,7 @@ let name ppf = Format.fprintf ppf "%s"
 let rec term ppf (t : Syntax.term) = term' ppf t.data
 
 and term' ppf = function
-  | Abs (x, t) -> Format.fprintf ppf "\\%a.%a" name x term t
+  | Abs (x, t) -> Format.fprintf ppf "(\\%a.%a)" name x term t
   | App (t1, t2) -> Format.fprintf ppf "%a %a" term t1 term t2
   | Var x -> name ppf x
 
