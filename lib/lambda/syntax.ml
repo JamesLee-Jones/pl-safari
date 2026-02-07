d18f8aa39470eed53bd5320e7325bde7be308f3d
James Lee-Jones <james@lee-jones.co.uk>
2026-02-07 19:39:10 +0000

    feat(test,lambda): add basic parser tests
    Add basic parser tests that check the round trip property of
    printing and parsing a parse tree.
    
    Fix a left recursion issue in the parsing of application.

--- lib/lambda/syntax.ml
+++ lib/lambda/syntax.ml
@@ -1,9 +1,12 @@
+open Base
 open Common
 
-type name = string
+type name = string [@@deriving sexp, compare]
 
 type term = term' Parsing.located
-and term' = Abs of name * term | App of term * term | Var of name
 
-type expr = Decl of name * term | Term of term
-type prog = expr list
+and term' = Abs of name * term | App of term * term | Var of name
+[@@deriving sexp, compare]
+
+type expr = Decl of name * term | Term of term [@@deriving sexp, compare]
+type prog = expr list [@@deriving sexp_of, compare]
