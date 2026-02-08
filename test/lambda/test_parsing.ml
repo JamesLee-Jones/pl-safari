c2f238f79dffde31b948b989a94416af4bc6a473
James Lee-Jones <james@lee-jones.co.uk>
2026-02-08 13:29:14 +0000

    feat(lambda,tests): switch to file unit tests
    Add cases directory to lambda tests containing both valid test cases and
    those containing syntax errors. This is more extensible than the AST
    based approach used previously.

--- test/lambda/test_parsing.ml
+++ test/lambda/test_parsing.ml
@@ -1,36 +1,58 @@
 open Format
-open Lexing
 
-let print_position outx lexbuf =
-  let pos = lexbuf.lex_curr_p in
-  fprintf outx "%s:%d:%d" pos.pos_fname pos.pos_lnum
-    (pos.pos_cnum - pos.pos_bol + 1)
+let assert_string_equal ~expected ~actual =
+  if not (String.equal expected actual) then
+    failwith
+      (Printf.sprintf "Strings differ!\n\nEXPECTED:\n%s\n\nACTUAL:\n%s\n"
+         expected actual)
 
-let round_trip prog =
-  let buff = Buffer.create 16 in
-  let ppf = formatter_of_buffer buff in
+let list_relative_directory dir =
+  let full_dir = Filename.concat (Sys.getcwd ()) dir in
+  if Sys.file_exists full_dir && Sys.is_directory full_dir then
+    let file_names = Array.to_list (Sys.readdir full_dir) in
+    List.map (fun fn -> Filename.concat full_dir fn) file_names
+  else failwith (Printf.sprintf "%s" full_dir)
 
-  Lambda.Print.prog ppf prog;
-  pp_print_flush ppf ();
-  let lexbuf = Lexing.from_string ~with_positions:true (Buffer.contents buff) in
-  Lambda.Parser.prog Lambda.Lexer.read lexbuf
+let test_directory test_fn dir = List.iter test_fn (list_relative_directory dir)
 
-let examples =
-  let open Lambda.Syntax in
-  let open Common.Parsing in
-  [
-    [];
-    [ Term (locate (Var "x")) ];
-    [ Term (locate (Var "x1y'")) ];
-    [ Term (locate (Abs ("x", locate (Var "y")))) ];
-    [ Term (locate (App (locate (Var "x"), locate (Var "y")))) ];
-    [
-      Term
-        (locate (App (locate (Abs ("x", locate (Var "x"))), locate (Var "y"))));
-    ];
-  ]
+(* Test the round trip property of parsing and printing *)
+let test_valid file_path =
+  (* Parse input file *)
+  let ic = open_in file_path in
+  let in_lexbuf = Lexing.from_channel ~with_positions:true ic in
+  let in_tree = Lambda.Parser.prog Lambda.Lexer.read in_lexbuf in
 
-let%test_unit "print_parse_round_trip" =
-  List.iter
-    (fun prog -> [%test_eq: Lambda.Syntax.prog] (round_trip prog) prog)
-    examples
+  (* Print to buffer *)
+  let in_buff = Buffer.create 16 in
+  let in_ppf = formatter_of_buffer in_buff in
+  Lambda.Print.prog in_ppf in_tree;
+
+  (* Reparse *)
+  let out_lexbuf =
+    Lexing.from_string ~with_positions:false (Buffer.contents in_buff)
+  in
+  let out_tree = Lambda.Parser.prog Lambda.Lexer.read out_lexbuf in
+
+  (* Reprint *)
+  let out_buff = Buffer.create 16 in
+  let out_ppf = formatter_of_buffer out_buff in
+  Lambda.Print.prog out_ppf out_tree;
+
+  (* Check printed results are equal *)
+  assert_string_equal ~expected:(Buffer.contents in_buff)
+    ~actual:(Buffer.contents out_buff)
+
+(* Test that parsing fails with a syntax error *)
+let test_syntax_error file_path =
+  (* Parse input file *)
+  let ic = open_in file_path in
+  let in_lexbuf = Lexing.from_channel ~with_positions:true ic in
+  try
+    let _ = Lambda.Parser.prog Lambda.Lexer.read in_lexbuf in
+    failwith "Expected syntax error"
+  with Lambda.Lexer.SyntaxError _ -> ()
+
+let%test_unit "test_round_trip" = test_directory test_valid "cases/valid"
+
+let%test_unit "test_syntax_error" =
+  test_directory test_syntax_error "cases/syntax_error"
