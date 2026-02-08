a62cddd4c0a7e41b8284024e0892aea542abc893
James Lee-Jones <james@lee-jones.co.uk>
2026-02-08 13:34:22 +0000

    refactor(lambda): remove 'base' dependency

--- lib/lambda/syntax.ml
+++ lib/lambda/syntax.ml
@@ -1,12 +1,9 @@
-open Base
 open Common
 
-type name = string [@@deriving sexp, compare]
+type name = string
 
 type term = term' Parsing.located
-
 and term' = Abs of name * term | App of term * term | Var of name
-[@@deriving sexp, compare]
 
-type expr = Decl of name * term | Term of term [@@deriving sexp, compare]
-type prog = expr list [@@deriving sexp_of, compare]
+type expr = Decl of name * term | Term of term
+type prog = expr list
